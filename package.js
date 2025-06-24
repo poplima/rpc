e43cf445b8c39de70f14aafb152d6d4213fd72cf
poplima <efstice7@gmail.com>
2025-06-24 19:46:48 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "discord-rich-presence-selfbot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord-rpc": "^3.2.0",
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": "16.x"
+  }
+}
