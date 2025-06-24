a3a300e3b7b16512c11966c59e826f439280f888
poplima <efstice7@gmail.com>
2025-06-24 20:21:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { Client } = require('discord.js-selfbot-v13');
+const express = require('express');
 require('dotenv').config();
 
 const client = new Client();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
